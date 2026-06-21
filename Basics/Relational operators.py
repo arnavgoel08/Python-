7a6c1e69b6bf41a140abbf80dd73075984b07981
Arnav Goel <goelarnav100@gmail.com>
2026-06-21 23:28:43 +0530

    Add relational operator example

---
+++ Basics/Relational operators.py
@@ -0,0 +1,9 @@
+a=9
+b=8
+
+print("Is a equal to b? \n",a==b)
+print("Is a not equal to b? \n",a!=b)
+print("Is a greater than b? \n",a>b)
+print("Is a less than b? \n",a<b)
+print("Is a greater than or equal to b? \n",a>=b)
+print("Is a less than or equal to b? \n",a<=b)
