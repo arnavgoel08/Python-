5deabf299c4c364c7aab2c06cb9cff2f3a945e43
Arnav Goel <goelarnav100@gmail.com>
2026-06-21 23:10:33 +0530

    Add basic aarthimetic operations

---
+++ Basics/Calculations.py
@@ -0,0 +1,13 @@
+a=1500
+b=1599
+sum=a+b
+print("sum is ",sum)
+
+diff =b-a
+print("difference is ",diff)
+
+product = a*b
+print("product is ",product)
+
+quotient = b/a
+print("quotient is ",quotient)
