b0225aecb664d3ea3499b89441010b2b7cc78351
Arnav Goel <goelarnav100@gmail.com>
2026-06-21 23:39:17 +0530

    Add assignment operator  practice set

---
+++ Basics/Assignment Operator.py
@@ -0,0 +1,27 @@
+n=10
+n=n+15
+print("n is ",n)
+
+s=15
+s+=10
+print("s is ",s)
+
+d=20
+d-=5
+print("d is ",d)
+
+p=250
+p*=2
+print("p is ",p)
+
+q=100
+q/=4
+print("q is ",q)
+
+r=100000
+r%=42
+print("r is ",r)
+
+e=5000
+e**=3
+print("e is ",e)
