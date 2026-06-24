cdba92d3f9f73671faecb8d07071bea6926dbdc1
Arnav Goel <goelarnav100@gmail.com>
2026-06-24 18:48:17 +0530

    Add Input practice set

---
+++ Basics/input_output.py
@@ -0,0 +1,7 @@
+name=input("Enter your name: ")
+age=int(input("Enter your age: "))
+percentage=float(input("Enter your percentage: "))
+
+print("Hello",name,"!")
+print("Your age is",age)
+print("Your percentage is",percentage)
