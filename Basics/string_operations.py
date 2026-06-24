2b39bdb18f5f06a21ad97a8c3309e7730d54b2f4
Arnav Goel <goelarnav100@gmail.com>
2026-06-24 23:25:16 +0530

    Add strings

---
+++ Basics/string_operations.py
@@ -0,0 +1,9 @@
+a="This is a string"
+b=' .Arnav'
+c=""" .This is another string"""
+
+str = a+b+c
+print(str)
+print("Length of string is :",len(str))
+print(str[15])
+print(str[31])
---
+++ Basics/strings.py
@@ -0,0 +1,9 @@
+a="This is a string"
+b=' Arnav'
+c=""" This is another string"""
+
+str = a+b+c
+print(str)
+
+str1="This is a string .\nI am creating it in python."
+print(str1)
