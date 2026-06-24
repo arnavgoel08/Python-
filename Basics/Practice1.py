8e6e8ce1c90fe65f68ba88da284c55a8727e4ffc
Arnav Goel <goelarnav100@gmail.com>
2026-06-24 20:49:39 +0530

    Add Practice set 1

---
+++ Basics/Practice1.py
@@ -0,0 +1,21 @@
+#Q1
+a=float(input("Enter a number: "))
+b=float(input("Enter another number: "))
+sum=a+b
+print ("The sum of the two numbers is :",sum)
+
+#Q2
+s=int(input("Enter side of a square: "))
+area=s**2
+print("Area of the square is :",area)
+
+#Q3
+c=float(input("Enter first number:"))
+d=float(input("Enter second number:"))
+avg=(c+d)/2
+print("Average of the two numbers is :",avg)
+
+#Q4
+e=int(input("Enter a number: "))
+f=int(input("Enter another number: "))
+print(e>=f)
