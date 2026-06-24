7b5d5fecc37e147d70c215b7565d66e099e39de7
Arnav Goel <goelarnav100@gmail.com>
2026-06-24 18:35:37 +0530

    Add Type Conversion practice set

---
+++ Basics/Type Conversion.py
@@ -0,0 +1,22 @@
+#Type Conversion in Python
+a = 1.9999
+b = 8
+sum = a + b
+print("The sum of a and b is:", sum)
+
+
+#Type Casting
+c="25"
+d=50
+e=int(c)
+f=float(c)
+
+sum1=e+d
+sum2=f+d
+
+print("The sum of c and d is:", sum1)
+print("The sum of f and d is:", sum2)
+
+g=3.14
+g=str(g)
+print(g,"\n",type(g))
